3893d01aac0f217d7d62a01df3f54e502ed54d08
Stet <steterik@gmail.com>
2022-10-07 17:35:16 +0600

    Part 2

---
+++ framework/App.js
@@ -0,0 +1,34 @@
+const http = require('http')
+const EventEmitter = require('events')
+
+module.exports = class App {
+  constructor(){
+    this.server = this._createServer()
+    this.emitter = new EventEmitter()
+  }
+  listen(port, callback){
+    this.server.listen(port, callback)
+  }
+  addRouter(router){
+    Object.keys(router.endpoints).forEach(path => {
+      const endpoint = router.endpoints[path]
+      Object.keys(endpoint).forEach(method => {
+        const handler = endpoint[method]
+        this.emitter.on(this._getRouteMask(path, method), (req, res) => {
+          handler(req, res)
+        })
+      })
+    })
+  }
+  _createServer(){
+    return http.createServer((req, res) => {
+      const emitted = this.emitter.emit(this._getRouteMask(req.url, req.method), req, res)
+      if(!emitted){
+        res.end('Error Page')
+      }
+    })
+  }
+  _getRouteMask(path, method) {
+    return `[${path}]:[${method}]`
+  }
+}
--- index.js
+++ index.js
@@ -1,43 +1,9 @@
 require('dotenv').config()
-const http = require('http')
-const EventEmitter = require('events')
-
-const emitter = new EventEmitter()
-
+const Router = require('./framework/Router')
+const App = require('./framework/App')
 const PORT = process.env.PORT || 3000
-
-class Router {
-  constructor(){
-    this.endpoints = {}
-  }
-  request(method = 'GET', path, handler){
-    if(!this.endpoints[path]){
-      this.endpoints[path] = {}
-    }
-    const endpoint = this.endpoints[path]
-    if(endpoint[method]){
-      throw new Error(`This ${method} in ${endpoint} already exists`)
-    }
-    endpoint[method] = handler
-    emitter.on(`[${path}]:[${method}]`, (req, res) => {
-      handler(req, res)
-    })
-  }
-  get(path, handler){
-    this.request('GET', path, handler)
-  }
-  post(path, handler){
-    this.request('POST', path, handler)
-  }
-  put(path, handler){
-    this.request('PUT', path, handler)
-  }
-  delete(path, handler){
-    this.request('DELETE', path, handler)
-  }
-}
-
 const router = new Router()
+const app = new App()
 
 router.get('/users', (req, res) => {
   res.end('Users page')
@@ -46,11 +12,5 @@ router.get('/posts', (req, res) => {
   res.end('Posts page')
 })
 
-const server = http.createServer((req, res) => {
-  const emitted = emitter.emit(`[${req.url}]:[${req.method}]`, req, res)
-  if(!emitted){
-    res.end('Error Page')
-  }
-})
-
-server.listen(PORT, () => console.log('Server has been created on PORT:' + PORT))
+app.addRouter(router)
+app.listen(PORT, () => console.log('Server has been created on PORT:' + PORT))
