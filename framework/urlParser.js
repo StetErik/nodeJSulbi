ce53a8ec1ab8fffb20c26d85f47939dcb52144b9
Stet <steterik@gmail.com>
2022-10-08 18:13:01 +0600

    Part 0

--- framework/urlParser.js
+++
@@ -1,7 +0,0 @@
-module.exports = (baseURL) => (req) => {
-  const parsedURL = new URL(req.url, baseURL)
-  req.pathname = parsedURL.pathname
-  const params = {}
-  parsedURL.searchParams.forEach((value, key) => params[key] = value)
-  req.params = params
-}
--- src/userRouter.js
+++
@@ -1,9 +0,0 @@
-const Router = require('./../framework/Router')
-const router = new Router()
-const { getPosts, createPost } = require('./userController')
-
-router.get('/users', getPosts)
-
-router.post('/users', createPost)
-
-module.exports = router
